a83572f185914bdf492a205f736adb1f78c6e058
Algorithman <algorithman.chris@googlemail.com>
2014-05-11 19:50:00 +0200

    Action repaired

--- src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/ActionMessage.cs
+++ src/SmokeLounge.AOtomation.Messaging/Messages/N3Messages/ActionMessage.cs
@@ -5,29 +5,29 @@ using System.Text;
 
 namespace SmokeLounge.AOtomation.Messaging.Messages.N3Messages
 {
+    using SmokeLounge.AOtomation.Messaging.GameData;
     using SmokeLounge.AOtomation.Messaging.Serialization;
     using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
 
     [AoContract((int)N3MessageType.Action)]
-    public class ActionMessage:N3Message
+    public class ActionMessage : N3Message
     {
         public ActionMessage()
         {
             this.N3MessageType = N3MessageType.Action;
         }
 
-  [AoMember(1)]
+        [AoMember(1)]
         [AoFlags("flag")]
         public int Unknown1 { get; set; }
 
-        [AoUsesFlags("flag",FlagsCriteria.HasAll,new[]{1})]
+        [AoUsesFlags("flag", typeof(int), FlagsCriteria.HasAny, new[] { 1 })]
+        [AoMember(2)]
+        public int Unknown2 { get; set; }
 
+        [AoUsesFlags("flag", typeof(int), FlagsCriteria.HasAny, new[] { 1 })]
+        [AoMember(3)]
+        public Identity Unknown3 { get; set; }
 
-/*
-Packet length: 45
- 01 05 00 0A 00 01 00 2D 00 00 0F 3E 00 32 BC 90     .......-...>.2..
- 20 49 52 7C 00 00 C7 6A 00 00 08 00 01 00 00 00      IR|...j........
- 01 00 00 00 66 00 00 C3 50 00 32 BC 90              ....f...P.2..
-*/
     }
 }
